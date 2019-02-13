587387fb757f4b3f2ddbdde2ec58f2de7b21bfdd
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-13 19:13:17 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(params.require(:student))
     @student.save
-    redirect_to student_path(params(@student))
+    redirect_to student_path(@student)
   end
 
   def show
